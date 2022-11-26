e3130b6ad9442940a12c54953241d2df4d60b949
vinuesa <vinuesa@ccg.unam.mx>
2022-11-25 20:46:52 -0600

    phyml_protModelFinder.sh v0.3_2022-11-25; computes sites/K, AICc and elapsed time

--- phyml_protModelFinder.sh
+++ phyml_protModelFinder.sh
@@ -15,8 +15,10 @@
 #set -euo pipefail # fails in some calls on tepeu with old phyml and bash; on buluc or tepeu use set -uo pipefail
 set -euo pipefail
 
+host=$(hostname)
+
 progname=${0##*/}
-version=0.2 # 2022-11-24; computes delta_BIC and BICw columns
+version=0.3 # 2022-11-25;
 min_bash_vers=4.3 # required to write modern bash idioms:
                   # 1.  printf '%(%F)T' '-1' in print_start_time; and 
                   # 2. passing an array or hash by name reference to a bash function (since version 4.3+), 
@@ -26,6 +28,7 @@ min_bash_vers=4.3 # required to write modern bash idioms:
 declare -a models       # array holding the base models (empirical substitution matrices to be evaluated)
 declare -A model_cmds   # hash holding the commands for each model 
 declare -A model_scores # hash holding the model lnL scores and AICi values 
+declare -A model_options
 
 # array of models to evaluate
 nuclear_genome_models=(AB BLOSUM62 DAYHOFF DCMut JTT LG VT WAG)
@@ -33,6 +36,13 @@ organelle_genemome_models=(CpREV MTMAM MtREV MtArt)
 nuclear_and_organellar=(AB BLOSUM62 DAYHOFF DCMut JTT LG VT WAG CpREV MTMAM MtREV MtArt)
 viral_genome_models=(HIVw HIVb RtREV)
 all_models=(AB BLOSUM62 DAYHOFF DCMut JTT LG VT WAG CpREV MTMAM MtREV MtArt HIVw HIVb RtREV)
+
+# hash mapping option => model_set
+model_options['1']='nuclear_genome_models'
+model_options['2']='organelle_genemome_models'
+model_options['3']='nuclear_and_organellar_models'
+model_options['4']='viral_genome_models'
+model_options['5']='all_models'
 
 #==============================#
 # >>> FUNCTION DEFINITIONS <<< #
@@ -81,6 +91,26 @@ function compute_AICi()
 }
 #-----------------------------------------------------------------------------------------
 
+function compute_AICc()
+{
+   local AIC score n_branches extra_params total_params
+   
+   score=$1
+   n_branches=$2
+   extra_params=$3
+   n_sites=$4
+   AIC=$5
+   
+   total_params=$((n_branches + extra_params))
+ 
+   # AICi=-2*lnLi + 2*Ni
+   #AIC=$( echo "(-2 * $score) + (2 * $total_params)" | bc -l )   
+   #echo $AIC + (2 * $total_params($total_params + 1)/($n_sites - $total_params -1)) | bc
+   
+   echo "$AIC + ( 2 * ($total_params * ($total_params + 1))/($n_sites - $total_params -1) )" | bc -l
+}
+#-----------------------------------------------------------------------------------------
+
 function compute_BIC()
 {
    local score n_branches extra_params total_params n_sites
@@ -125,6 +155,12 @@ EoH
 
 }
 #-----------------------------------------------------------------------------------------
+#============================= END FUNCTION DEFINITIONS ==================================
+#=========================================================================================
+
+# ============ #
+# >>> MAIN <<<
+# ------------ #
 
 # 0. Check that the input file was provided, and that the host runs bash >= v4.3
 (( $# != 2 )) && print_help
@@ -137,8 +173,15 @@ wkd=$(pwd)
 # verify input & bash vesion
 [[ ! -s "$infile" ]] && echo "FATAL ERROR: could not find $infile in $wkd" && exit 1
 (( model_set < 1 )) || ((model_set > 5 )) && print_help
-check_bash_version "$min_bash_vers"
 check_is_phylip "$infile"
+
+# OK, ready to start the analysis ...
+start_time=$SECONDS
+echo "================================================================================"
+check_bash_version "$min_bash_vers"
+echo -n "# $progname v$version run on $host started on: "; printf '%(%F at %T)T\n' '-1'
+echo "================================================================================"
+echo
 
 # 1. get sequence stats
 no_seq=$(awk 'NR == 1{print $1}' "$infile") 
@@ -158,6 +201,7 @@ esac
 # 3. Compute a fast NJ tree estimating distances with the LG matrix 
 print_start_time 
 echo "1. Computing NJ-LG tree for input file $infile with $no_seq sequences"
+echo '--------------------------------------------------------------------------------'
 phyml -i "$infile" -d aa -m LG -c 1 -b 0 -o n &> /dev/null
 
 # 2. rename the outfile for future use as usertree
@@ -169,16 +213,18 @@ fi
 
 # 4. run a for loop to combine all base models with (or not) +G and or +f
 #     and fill the model_scores and model_cmds hashes
-echo "2. running in a for loop to combine all base models in model_set $model_set with (or not) +G and or +f"
+echo "2. running in a for loop to combine all base models in model_set ${model_set}=>${model_options[$model_set]}, with (or not) +G and or +f"
 for mat in "${models[@]}"; do
      print_start_time && echo "# running: phyml -i $infile -d aa -m $mat -u ${infile}_LG-NJ.nwk -c 1 -v 0 -o lr &> /dev/null"
      phyml -i "$infile" -d aa -m "$mat" -u "${infile}"_LG-NJ.nwk -c 1 -o lr &> /dev/null 
      extra_params=0 
      total_params=$((no_branches + extra_params))
+     sites_by_K=$(echo 'scale=2;'"$no_sites/$total_params" | bc -l)
      score=$(awk '/Log-/{print $3}' "${infile}"_phyml_stats.txt)
      AICi=$(compute_AICi "$score" "$no_branches" "$extra_params")
+     AICc=$(compute_AICc "$score" "$no_branches" "$extra_params" "$no_sites" "$AICi")
      BICi=$(compute_BIC "$score" "$no_branches" "$extra_params" "$no_sites")
-     printf -v model_string "%d\t%.5f\t%.5f\t%.5f" "$total_params" "$score" "$AICi" "$BICi"
+     printf -v model_string "%d\t%.5f\t%.5f\t%.5f\t%.5f\t%.5f" "$total_params" "$sites_by_K" "$score" "$AICi" "$AICc" "$BICi"
      model_scores["${mat}"]="$model_string"
      model_cmds["${mat}"]="$mat"
 
@@ -186,10 +232,12 @@ for mat in "${models[@]}"; do
      phyml -i "$infile" -d aa -m "${mat}" -c 4 -a e -u "${infile}"_LG-NJ.nwk -o lr &> /dev/null     
      extra_params=1 
      total_params=$((no_branches + extra_params))
+     sites_by_K=$(echo 'scale=2;'"$no_sites/$total_params" | bc -l)
      score=$(awk '/Log-/{print $3}' "${infile}"_phyml_stats.txt)
      AICi=$(compute_AICi "$score" "$no_branches" "$extra_params")
+     AICc=$(compute_AICc "$score" "$no_branches" "$extra_params" "$no_sites" "$AICi")
      BICi=$(compute_BIC "$score" "$no_branches" "$extra_params" "$no_sites")
-     printf -v model_string "%d\t%.5f\t%.5f\t%.5f" "$total_params" "$score" "$AICi" "$BICi"
+     printf -v model_string "%d\t%.5f\t%.5f\t%.5f\t%.5f\t%.5f" "$total_params" "$sites_by_K" "$score" "$AICi" "$AICc" "$BICi"
      model_scores["${mat}+G"]="$model_string"
      model_cmds["${mat}+G"]="$mat -c 4 -a e"
 
@@ -197,10 +245,12 @@ for mat in "${models[@]}"; do
      phyml -i "$infile" -d aa -m "$mat" -f e -c 1 -u "${infile}"_LG-NJ.nwk -o lr &> /dev/null     
      extra_params=19 #19 from AA frequencies
      total_params=$((no_branches + extra_params))
+     sites_by_K=$(echo 'scale=2;'"$no_sites/$total_params" | bc -l)
      score=$(awk '/Log-/{print $3}' "${infile}"_phyml_stats.txt)
      AICi=$(compute_AICi "$score" "$no_branches" "$extra_params")
+     AICc=$(compute_AICc "$score" "$no_branches" "$extra_params" "$no_sites" "$AICi")
      BICi=$(compute_BIC "$score" "$no_branches" "$extra_params" "$no_sites")
-     printf -v model_string "%d\t%.5f\t%.5f\t%.5f" "$total_params" "$score" "$AICi" "$BICi"
+     printf -v model_string "%d\t%.5f\t%.5f\t%.5f\t%.5f\t%.5f" "$total_params" "$sites_by_K" "$score" "$AICi" "$AICc" "$BICi"
      model_scores["${mat}+f"]="$model_string"
      model_cmds["${mat}+f"]="$mat -f e"
 
@@ -208,30 +258,32 @@ for mat in "${models[@]}"; do
      phyml -i "$infile" -d aa -m "$mat" -u "${infile}"_LG-NJ.nwk -f e -a e -c 4 -o lr &> /dev/null     
      extra_params=20 #19 from AA frequencies + 1 gamma 
      total_params=$((no_branches + extra_params))
+     sites_by_K=$(echo 'scale=2;'"$no_sites/$total_params" | bc -l)
      score=$(awk '/Log-/{print $3}' "${infile}"_phyml_stats.txt)
      AICi=$(compute_AICi "$score" "$no_branches" "$extra_params")
+     AICc=$(compute_AICc "$score" "$no_branches" "$extra_params" "$no_sites" "$AICi")
      BICi=$(compute_BIC "$score" "$no_branches" "$extra_params" "$no_sites")
-     printf -v model_string "%d\t%.5f\t%.5f\t%.5f" "$total_params" "$score" "$AICi" "$BICi"
+     printf -v model_string "%d\t%.5f\t%.5f\t%.5f\t%.5f\t%.5f" "$total_params" "$sites_by_K" "$score" "$AICi" "$AICc" "$BICi"
      model_scores["${mat}+f+G"]="$model_string"
      model_cmds["${mat}+f+G"]="$mat -f e -c 4 -a e"
 done
-echo '-----------------------------------------------------------------------------'
+echo '--------------------------------------------------------------------------------------------------'
 echo
 
-
 # 5. print a sorted summary table of model fits from the model_scores hash
-print_start_time 
+print_start_time
+
 echo "# writing ${infile}_sorted_model_set_${model_set}_fits.tsv, sorted by BIC"
 for m in "${!model_scores[@]}"; do
     echo -e "$m\t${model_scores[$m]}"
-done | sort -nk5 > "${infile}"_sorted_model_set_"${model_set}"_fits.tsv
+done | sort -nk7 > "${infile}"_sorted_model_set_"${model_set}"_fits.tsv
 
 
 # 6 compute delta_BIC and BICw, based on "${infile}"_sorted_model_set_"${model_set}"_fits.tsv
 declare -a BIC_a
 declare -a BIC_deltas_a
 declare -a BICw_a
-BIC_a=( $(awk '{print $5}' "${infile}"_sorted_model_set_"${model_set}"_fits.tsv) )
+BIC_a=( $(awk '{print $7}' "${infile}"_sorted_model_set_"${model_set}"_fits.tsv) )
 min_BIC="${BIC_a[0]}"
 
 # 6.1 fill BIC_deltas_a array
@@ -274,8 +326,8 @@ if [[ -s "${infile}"_sorted_model_set_"${model_set}"_fits.tsv ]]; then
     [[ -z "$best_model" ]] && echo "FATAL ERROR: unbound \$best_model at $LINENO" && exit 1
 
     # print table with header to STDOUT and save to file
-    awk 'BEGIN{print "model\tK\tlnL\tAIC\tBIC\tdeltaBIC\tBICw"}{print}' "${infile}"_sorted_model_set_"${model_set}"_fits.tsv | column -t
-    awk 'BEGIN{print "model\tK\tlnL\tAIC\tBIC\tdeltaBIC\tBICw"}{print}' "${infile}"_sorted_model_set_"${model_set}"_fits.tsv > t
+    awk 'BEGIN{print "model\tK\tsites/K\tlnL\tAIC\tAICc\tBIC\tdeltaBIC\tBICw"}{print}' "${infile}"_sorted_model_set_"${model_set}"_fits.tsv | column -t
+    awk 'BEGIN{print "model\tK\tsites/K\tlnL\tAIC\tAICc\tBIC\tdeltaBIC\tBICw"}{print}' "${infile}"_sorted_model_set_"${model_set}"_fits.tsv > t
     mv t "${infile}"_sorted_model_set_"${model_set}"_fits.tsv
 else
     echo "ERROR: could not write ${infile}_sorted_model_set_${model_set}_fits.tsv"
@@ -287,7 +339,8 @@ fi
 
 
 # 7. compute ML tree under best-fitting model
-echo '-----------------------------------------------------------------------------'
+echo '--------------------------------------------------------------------------------------------------'
+echo "* NOTE: when sites/K < 40, the AICc is recommended over AIC."
 echo
 echo "... will estimate the ML tree under best-fitting model $best_model selected by BIC"
 
@@ -312,5 +365,14 @@ if [[ -s "${infile}"_phyml_tree.txt ]]; then
 else
      echo "FATAL ERROR: ${infile}_phyml_tree.txt was not generated!"
 fi
+echo '--------------------------------------------------------------------------------------------------'
 
+echo
+
+elapsed=$(( SECONDS - start_time ))
+
+eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days, %H hr, %M min, %S sec')"
+
+echo 'Done!'
+echo
 exit 0
